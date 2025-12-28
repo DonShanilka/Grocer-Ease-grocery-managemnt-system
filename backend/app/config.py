8d7c0f7af232ed008451f92a65144b518eb957de
DonShanilka <shanilka799@gmail.com>
2025-12-28 19:32:19 +0530

    update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:Shanilka800%40%23@localhost:3306/gorcer_ease'
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}@"
+        f"{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DB')}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
