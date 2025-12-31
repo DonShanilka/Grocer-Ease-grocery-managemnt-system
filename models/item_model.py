6b49d5a970785f5fe421d1867639dc21121fa339
DonShanilka <shanilka799@gmail.com>
2025-12-31 19:14:45 +0530

    update item model

--- models/item_model.py
+++ models/item_model.py
@@ -1,4 +1,12 @@
 class Item:
-    def __init__(self, id=None, name=None):
+    def __init__(self, id=None, name=None, category=None, price=None, quantity=None, unit=None, description=None, supplier=None, status=None, added_date=None):
         self.id = id
         self.name = name
+        self.category = category
+        self.price = price
+        self.quantity = quantity
+        self.unit = unit
+        self.description = description
+        self.supplier = supplier
+        self.status = status
+        self.added_date = added_date
