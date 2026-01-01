ecb5c63ea9e5fe8ab0d018755ea852d0fb3e445a
DonShanilka <shanilka799@gmail.com>
2026-01-01 17:41:32 +0530

    create add_customer in customer controller

---
+++ controller/customer_controller.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, request, jsonify
+from flask_cors import cross_origin
+from services.customer_service import CustomerService
+
+customer_bp = Blueprint("customer_bp", __name__)
+
+@customer_bp.route("/customers", methods=["POST"])
+@cross_origin()
+def add_customer():
+    try:
+        data = request.get_json()
+
+        customer = CustomerService.add_customer(data)
+        return jsonify({
+            "message": "Customer saved successfully",
+            "customer": {
+                "name": customer.name,
+                "email": customer.email,    
+                "phone": customer.phone,
+                "address": customer.address,
+                "item_name": customer.item_name,
+                "qty": customer.qty
+            } 
+        }), 201
+        
+    except ValueError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        print("SERVER ERROR:", e)
+        return jsonify({"error": "Internal server error"}), 500
