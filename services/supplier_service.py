4ce291fd5c85664ac48123ac1dbc8120beacfdd3
DonShanilka <shanilka799@gmail.com>
2026-01-02 20:46:18 +0530

    create add_supplier def in supplier service

---
+++ services/supplier_service.py
@@ -0,0 +1,23 @@
+from models.Supplier_model import Supplier
+from repositories.supplier_repository import SupplierRepository
+
+class SupplierService:
+    
+    @staticmethod
+    def add_supplier(data):
+        if not data or not data.get("name"):
+            raise ValueError("Supplier name is required")
+        
+        supplier  = Supplier(
+            name = data.get("name"),
+            phone = data.get("phone"),
+            email = data.get("email"),
+            address = data.get("address"),
+            supplied_items = data.get("supplied_items"),
+            price_per_unit = data.get("price_per_unit"),
+            qty = data.get("qty"),
+            status = data.get("status", "active")
+        )
+        
+        SupplierRepository.save(supplier)
+        return supplier
