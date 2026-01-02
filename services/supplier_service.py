68bb924c2d42e1da2b254d696cd84028632a109a
DonShanilka <shanilka799@gmail.com>
2026-01-02 20:50:14 +0530

    create update_supplier def in supplier service

--- services/supplier_service.py
+++ services/supplier_service.py
@@ -21,3 +21,24 @@ class SupplierService:
         
         SupplierRepository.save(supplier)
         return supplier
+    
+    
+    @staticmethod
+    def update_supplier(supplier_id, data):
+        if not data.get("name"):
+            raise ValueError("Supplier name is required")
+        
+        supplier  = Supplier(
+            name = data.get("name"),
+            phone = data.get("phone"),
+            email = data.get("email"),
+            address = data.get("address"),
+            supplied_items = data.get("supplied_items"),
+            price_per_unit = data.get("price_per_unit"),
+            qty = data.get("qty"),
+            status = data.get("status")
+        )
+        
+        updated = SupplierRepository.update(supplier_id, supplier)
+        if updated == 0:
+            raise ValueError("Supplier not found")
