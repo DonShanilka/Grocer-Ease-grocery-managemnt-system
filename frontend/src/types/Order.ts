af10a44b0b97a7fdcdae18e97a0402a62c40bc53
DonShanilka <shanilka799@gmail.com>
2026-01-14 23:16:09 +0530

    create orderTs

---
+++ frontend/src/types/Order.ts
@@ -0,0 +1,16 @@
+export interface OrderItem {
+  item_id: number;
+  item_name: string;
+  price: number;
+  quantity: number;
+}
+
+export interface Order {
+  id: number;
+  customer_name: string;
+  order_type: "TAKEAWAY" | "DELIVERY";
+  payment_type: "CASH" | "CARD";
+  status: string;
+  total_amount: number;
+  created_at: string;
+}
