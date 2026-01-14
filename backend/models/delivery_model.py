f784ee77c35a2414f28d19ca02f73f27707d1d89
DonShanilka <shanilka799@gmail.com>
2026-01-14 17:12:53 +0530

    update delivery model

--- backend/models/delivery_model.py
+++ backend/models/delivery_model.py
@@ -1,22 +1,7 @@
 class Delivery:
-    def __init__(
-        self,
-        order_id,
-        address,
-        phone,
-        delivery_person=None
-    ):
+    def __init__(self, order_id, delivery_address, contact_phone, delivery_status="PENDING", assigned_driver=None):
         self.order_id = order_id
-        self.address = address
-        self.phone = phone
-        self.delivery_person = delivery_person
-
-        self.delivery_status = "PENDING"
-        self.estimated_time = None
-
-    def assign_delivery_person(self, name):
-        self.delivery_person = name
-        self.delivery_status = "ASSIGNED"
-
-    def mark_delivered(self):
-        self.delivery_status = "DELIVERED"
+        self.address = delivery_address
+        self.phone = contact_phone
+        self.delivery_status = delivery_status
+        self.delivery_person = assigned_driver
